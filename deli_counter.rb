f9c26bb1888f1d6eabfb03caa14396a46b516b8f
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-16 01:59:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,9 +16,11 @@ end
 
 
 def now_serving(katz_deli)
-  if katz_deli.size == 0 
+  if katz_deli.size > 0 
+    puts "Currently serving #{katz_deli(0)}."
+    katz_deli.shift 
+    
+  elsif katz_deli.size == 0 
     puts "There is nobody waiting to be served!"
-  
-  elsif 
   end
 end
