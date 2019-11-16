05f00711caf102fae567ddcec92e62a9aac12f21
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-16 02:01:53 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def line(deli)
+ 
+  if deli.length == 0 
+    puts "The line is currently empty."
+  
+  elsif deli.length > 0 
+    puts "The line is currently: " + deli.map.with_index {|name, i| "#{i + 1}. #{name}"}.join(" ")
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+  katz_deli.append(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+end
+
+
+def now_serving(katz_deli)
+  if katz_deli.size > 0 
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift 
+    
+  elsif katz_deli.size == 0 
+    puts "There is nobody waiting to be served!"
+  end
+end
